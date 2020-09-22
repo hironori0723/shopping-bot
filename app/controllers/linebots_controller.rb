abc81894c307dd8e8558c3ea98c70bc2b85f87b7
hironori0723 <604849hiro.s@gmail.com>
2020-09-22 14:39:40 +0900

    訂正＃３

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,7 +1,8 @@
 class LinebotsController < ApplicationController
   require 'line/bot'
 
-  protect_from_forgery except:[:callback]
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery except: [:callback]
 
   def callback
     body = request.body.read
@@ -15,9 +16,9 @@ class LinebotsController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-
+          # 入力した文字をinputに格納
           input = event.message['text']
-
+          # search_and_create_messageメソッド内で、楽天APIを用いた商品検索、メッセージの作成を行う
           message = search_and_create_message(input)
           client.reply_message(event['replyToken'], message)
         end
@@ -28,99 +29,101 @@ class LinebotsController < ApplicationController
 
   private
 
-    def client
-      @client ||= Line::Bot::Client.new do |config|
-        config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
-        config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
-      end
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
     end
+  end
 
-    def search_and_create_message(input)
-      RakutenWebService.configure do |c|
-        c.application_id = ENV['RAKUTEN_APPID']
-        c.affiliate_id = ENV['RAKUTEN_AFID']
-      end
-
-      res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 3, imageFlag: 1)
-      items = []
-      items = res.map{|item| item}
-      make_reply_content(items)
+  def search_and_create_message(input)
+    RakutenWebService.configure do |c|
+      c.application_id = ENV['RAKUTEN_APPID']
+      c.affiliate_id = ENV['RAKUTEN_AFID']
     end
+    # 楽天の商品検索APIで画像がある商品の中で、入力値で検索して上から3件を取得する
+    # 商品検索+ランキングでの取得はできないため標準の並び順で上から3件取得する
+    res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 3, imageFlag: 1)
+    items = []
+    # 取得したデータを使いやすいように配列に格納し直す
+    items = res.map{|item| item}
+    make_reply_content(items)
+  end
 
-    def make_reply_content(items)
+  def make_reply_content(items)
+    {
+      "type": 'flex',
+      "altText": 'This is a Flex Message',
+      "contents":
       {
-        "type": 'flex',
-        "altText": 'This is a Flex Message',
-        "contents":
-        {
-          "type": 'carousel',
-          "contents": [
-            make_part(items[0]),
-            make_part(items[1]),
-            make_part(items[2])
-          ]
-        }
+        "type": 'carousel',
+        "contents": [
+          make_part(items[0]),
+          make_part(items[1]),
+          make_part(items[2])
+        ]
       }
-    end
+    }
+  end
 
-    def make_part(item)
-      title = item['itemName']
-      price = item['itemPrice'].to_s + "円"
-      url = item['itemUrl']
-      image = item['mediumImageUrls'].first
+  def make_part(item)
+    title = item['itemName']
+    price = item['itemPrice'].to_s + '円'
+    url = item['itemUrl']
+    image = item['mediumImageUrls'].first
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "url": image
+      },
+      "body":
       {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "url": image
-        },
-        "body":
-        {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": title,
-              "wrap": true,
-              "weight": "bold",
-              "size": "lg"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "contents": [
-                {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": title,
+            "wrap": true,
+            "weight": "bold",
+            "size": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": [
+              {
                 "type": "text",
                 "text": price,
                 "wrap": true,
                 "weight": "bold",
                 "flex": 0
-                }
-              ]
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "primary",
-              "action": {
-                "type": "url",
-                "label": "楽天市場商品ページへ",
-                "url": url
               }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "Amazon商品ページへ",
+              "uri": url
             }
-          ]
-        }
+          }
+        ]
       }
-    end
+    }
+  end
 end
